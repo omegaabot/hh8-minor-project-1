84d1f414d0fb9e78bf10d214b0f4fc1805ad2a79
Aditya Raj <120517393+omegaabot@users.noreply.github.com>
2026-02-02 00:51:16 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration file for storing API keys and project settings 
+
+# AbuseIPDB API key
+ABUSEIPDB_API_KEY = "YOUR_ABUSEIPDB_API_KEY"
+
+# Additional keys for other services (e.g., VirusTotal, if used)
+VIRUSTOTAL_API_KEY = "YOUR_VIRUSTOTAL_API_KEY"
